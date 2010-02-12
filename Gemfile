24bd7e727753a8df50448b0a66ebf09a3f979f95
Schmurfy <j.ammous@gmail.com>
2010-02-12 16:49:21 +0100

    added simple script to handle alerts from collectd

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 
-bundle_path 'gems'
-bin_path 'gems/bin'
+source 'http://gemcutter.org'
 
 gem 'sinatra',      '>= 0.9.4'
 gem 'rack',         '>= 1.1.0'
 gem 'errand',       :git => 'git://github.com/schmurfy/rubyrrdtool.git'
-gem 'yajl-ruby',    '>= 0.6.4', :require_as => 'yajl'
+gem 'yajl-ruby',    '>= 0.6.4', :require => 'yajl'
 gem 'haml',         '>= 2.2.17'
-
+gem 'tzinfo'
+gem 'mmmail'
 
---
+++ handle_alerts.rb
@@ -0,0 +1,119 @@
+#!/usr/bin/env ruby1.9
+
+# this file is designed to be run by collectd by:
+# <Plugin exec>
+#   NotificationExec "user" "/path/to/handle_alerts.rb"
+# </Plugin>
+#
+# input time are supposed to be in GMT
+#
+# alerts will be logged in file and emails will be sent
+# to each configured recipients
+#
+require 'logger'
+require 'yaml'
+
+require File.join(File.dirname(__FILE__), 'gems/environment')
+Bundler.require_env()
+
+
+module Config
+  def self._data
+    @config ||= YAML.parse_file( File.join(File.dirname(__FILE__), 'config/alerts_conf.yml') )
+  end
+  
+  def self.timezone
+    @tz ||= TZInfo::Timezone.get(_data['timezone'].value)
+  end
+  
+  def self.email_recipients
+    @email_list ||= _data['emails']['recipients'].value.map(&:value)
+  end
+  
+  def self.from_email
+    @email_from ||= _data['emails']['from'].value
+  end
+  
+  def self.smtp_host
+    @smtp_host ||= _data['emails']['host'].value
+  end
+end
+
+log_file = Config._data['logger']['path'].value
+logger = Logger.new( log_file )
+
+MmMail::Transport::DefaultConfig.host = Config::smtp_host
+
+collectd_interval = ENV['COLLECTD_INTERVAL']
+collectd_hostname = ENV['COLLECTD_HOSTNAME']
+
+def send_email(subject, body)
+  Config::email_recipients.each do |recipient|
+    MmMail.mail(
+        :to       => recipient,
+        :from     => Config::from_email,
+        :subject  => subject,
+        :body     => body,
+      )
+    end
+end
+
+class Notification
+  
+  FLOAT_VALUES = [:value, :warn_min, :warn_max, :failure_min, :failure_max].freeze
+  
+  attr_accessor :severity, :time, :host, :plugin, :type, :type_instance, :datasource, :value,
+    :warn_min, :warn_max, :failure_min, :failure_max, :message
+  
+  def initialize(md)
+    md.names.each do |key|
+      if FLOAT_VALUES.include?(key.to_sym)
+        send("#{key}=", md[key.to_sym].to_f)
+      else
+        send("#{key}=", md[key.to_sym])
+      end
+    end
+    
+    t = Time.at(self.time.to_i)
+    self.time = Config::timezone.utc_to_local(t)
+  end
+end
+
+class NotificatioParser
+  REGEXP = %r{\
+Severity: (?<severity>[^\n]+)\n\
+Time: (?<time>[0-9]+)\n\
+Host: (?<host>[^\n]+)\n\
+Plugin: (?<plugin>[^\n]+)\n\
+Type: (?<type>[^\n]+)\n\
+TypeInstance: (?<type_instance>[^\n]+)\n\
+DataSource: (?<datasource>[^\n]+)\n\
+CurrentValue: (?<value>[^\n]+)\n\
+WarningMin: (?<warn_min>[^\n]+)\n\
+WarningMax: (?<warn_max>[^\n]+)\n\
+FailureMin: (?<failure_min>[^\n]+)\n\
+FailureMax: (?<failure_max>[^\n]+)\n\
+\n\
+(?:(?<message>[^\n]+)\n)?\
+}
+  
+  def self.parse(str)
+    m = REGEXP.match(str)
+    m ? Notification.new(m) : nil
+  end
+  
+end
+
+while data = $stdin.readpartial(4096)
+  File.open('/tmp/notification', 'w'){|f| f.write(data) }
+  
+  if ev = NotificatioParser.parse(data)
+    logger.debug("[#{ev.time.strftime('%H:%m:%S')} - #{ev.host}] #{ev.severity} ")
+    send_email("#{ev.host} - #{ev.plugin}:#{ev.type}:#{ev.type_instance} - #{ev.severity}", %{\
+      Current Value: #{ev.value}
+      Warning thresholds: #{ev.warn_min} - #{ev.warn_max}
+      Failure thresholds: #{ev.failure_min} - #{ev.failure_max}
+    })
+  end
+end
+
