4700827793dcb9041c94cb33fe391f0bf5181cad
Schmurfy <j.ammous@gmail.com>
2010-03-16 23:44:04 +0100

    added authentication, wow !

--- main.rb
+++ main.rb
@@ -27,14 +27,9 @@ template :layout do
   File.read(File.join(__DIR__, 'views/layout.haml'))
 end
 
-# infrastructure for embedding
-# get '/javascripts/visage.js' do
-#   javascript = ""
-#   %w{raphael-min g.raphael g.line mootools-1.2.3-core mootools-1.2.3.1-more graph}.each do |js|
-#     javascript += File.read(File.join(__DIR__, 'public', 'javascripts', "#{js}.js"))
-#   end
-#   javascript
-# end
+use Rack::Auth::Basic do |username, password|
+  [username, password] == ['admin', 'admin']
+end
 
 
 get '/' do 
